90500c483f74ff203184dc336de4b2edca412eea
thomp276 <thomp276@wwu.edu>
2020-02-16 00:21:19 -0800

    trying lua

---
+++ entropy.lua
@@ -0,0 +1,21 @@
+function init (args)
+    local needs = {}
+    needs["protocol"] = "dns"
+    return needs
+end
+
+function match(args)
+    a = tostring(args)
+    print(a)
+    --[[
+    if #a > 0 then
+        if a:find("^POST%s+/.*%.php%s+HTTP/1.0$") then
+            return 1
+        end
+    end
+    --]]
+
+    return 0
+end
+
+return 0

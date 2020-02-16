b4cf0ade8b34c5478d518df58d8c81daae2f2986
edwardjthompson <edward.j.thompson@gmail.com>
2020-02-16 13:30:59 -0800

    rm if nil check

--- entropy.lua
+++ entropy.lua
@@ -7,40 +7,40 @@ end
 function match(args)
     -- print(args)
     dns_query = DnsGetQueries();
-    if dns_query ~= nil then
-        for n, t in pairs(dns_query) do
-            rrname = t["rrname"]
-            rrtype = t["type"]
+    -- if dns_query ~= nil then
+    for n, t in pairs(dns_query) do
+        rrname = t["rrname"]
+        rrtype = t["type"]
 
-            print ("QUERY: " .. ts .. " " .. rrname .. " [**] " .. rrtype .. " [**] " ..
-                "TODO" .. " [**] " .. srcip .. ":" .. sp .. " -> " ..
-                dstip .. ":" .. dp)
-        end
+        print ("QUERY: " .. ts .. " " .. rrname .. " [**] " .. rrtype .. " [**] " ..
+            "TODO" .. " [**] " .. srcip .. ":" .. sp .. " -> " ..
+            dstip .. ":" .. dp)
     end
+    -- end
     dns_answers = DnsGetAnswers();
-    if dns_answers ~= nil then
-        for n, t in pairs(dns_answers) do
-            rrname = t["rrname"]
-            rrtype = t["type"]
-            ttl = t["ttl"]
+    -- if dns_answers ~= nil then
+    for n, t in pairs(dns_answers) do
+        rrname = t["rrname"]
+        rrtype = t["type"]
+        ttl = t["ttl"]
 
-            print ("ANSWER: " .. ts .. " " .. rrname .. " [**] " .. rrtype .. " [**] " ..
-                ttl .. " [**] " .. srcip .. ":" .. sp .. " -> " ..
-                dstip .. ":" .. dp)
-        end
+        print ("ANSWER: " .. ts .. " " .. rrname .. " [**] " .. rrtype .. " [**] " ..
+            ttl .. " [**] " .. srcip .. ":" .. sp .. " -> " ..
+            dstip .. ":" .. dp)
     end
+    -- end
     dns_auth = DnsGetAuthorities();
-    if dns_auth ~= nil then
-        for n, t in pairs(dns_auth) do
-            rrname = t["rrname"]
-            rrtype = t["type"]
-            ttl = t["ttl"]
+    -- if dns_auth ~= nil then
+    for n, t in pairs(dns_auth) do
+        rrname = t["rrname"]
+        rrtype = t["type"]
+        ttl = t["ttl"]
 
-            print ("AUTHORITY: " .. ts .. " " .. rrname .. " [**] " .. rrtype .. " [**] " ..
-                ttl .. " [**] " .. srcip .. ":" .. sp .. " -> " ..
-                dstip .. ":" .. dp)
-        end
+        print ("AUTHORITY: " .. ts .. " " .. rrname .. " [**] " .. rrtype .. " [**] " ..
+            ttl .. " [**] " .. srcip .. ":" .. sp .. " -> " ..
+            dstip .. ":" .. dp)
     end
+    -- end
     rcode = DnsGetRcode();
     if rcode == nil then
         return 0

ff319c1d8473a84b8eac91cfee99e2e41d20da6c
thomp276 <thomp276@wwu.edu>
2020-02-16 01:07:44 -0800

    trying to print dns query

--- entropy.lua
+++ entropy.lua
@@ -6,6 +6,7 @@ end
 
 function match(args)
     dns_query = DnsGetQueries();
+    print(tostring(dns_query))
     if dns_query ~= nil then
         for n, t in pairs(dns_query) do
             rrname = t["rrname"]

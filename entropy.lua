a28aa2478220d25727cd68b392378aeff8d5b975
edwardjthompson <edward.j.thompson@gmail.com>
2020-02-16 23:47:19 -0800

    strucutre of lua script

--- entropy.lua
+++ entropy.lua
@@ -6,7 +6,12 @@ end
 
 function match(args)
     p = SCPacketPayload()
-    print(p)
-    return 1  
+    --regex to find domain name
+    --calculate max entropy
+    --calculate entroy
+    --if entropy > 3 and 85% max 
+    return 1 
+    --else
+    return 0
 end
 
